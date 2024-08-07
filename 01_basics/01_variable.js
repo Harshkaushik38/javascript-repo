9fe24848b9b55479a0eca69c603712573f63d15a
Harshkaushik38 <harshkaushik6289@gmail.com>
2024-08-07 05:10:40 +0000

    add new file

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+
+const accountId = 1234;
+let email = "harsh000@gmail";
+var aacountPassword = "jaipur";
+let accountstate;
+
+
+console.log(email);
+
+console.table([accountId,email,aacountPassword,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+
+let name ="harsh";
+
+console.log(typeof "harsh");
