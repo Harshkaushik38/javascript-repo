7f8fe970305a8db0cc28e1bb33225d4fae4ab9d6
Harshkaushik38 <harshkaushik6289@gmail.com>
2024-08-09 03:50:32 +0000

    add new text

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+
+
+let score = 75;
+
+// console.log(typeof score);
+
+
+let value = String(score);
+// console.log(typeof value);
+
+
+// "33" => 33;
+// "33abc" => NaN;
+// true => 1; false => 0
+
+
+let isloggedIn = "";
+
+let booleanisloggedIn = Boolean(isloggedIn);
+// console.log(booleanisloggedIn)
+
+// 1 =>true , 0 => false
+// "" => false;
+// "harsh" => true;
+
+let somenumber = 33;
+
+let stringnumber = String(somenumber);
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
